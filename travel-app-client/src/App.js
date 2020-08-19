5930597dfdcdb5dc80bd1abc8dd264e357382a45
ksyasuda <ksyasuda@umich.edu>
2020-08-19 00:13:49 -0700

    Add button to go to website (for now it routes to localhost)

--- travel-app-client/src/App.js
+++ travel-app-client/src/App.js
@@ -1,32 +1,60 @@
-import React, {Component} from 'react';
-import './App.css';
-import MyMap from './containers/MyMap/MyMap';
-import {BrowserRouter} from 'react-router-dom';
-import {Link, Route, Switch} from 'react-router-dom';
-import AllLocations from './containers/AllLocations/AllLocations'
+import React, { Component } from "react";
+import "./App.css";
+import MyMap from "./containers/MyMap/MyMap";
+import { BrowserRouter } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
+import AllLocations from "./containers/AllLocations/AllLocations";
 // import Button from './components/UI/Button/Button';
-
 
 class App extends Component {
 	state = {
 		useDarkMode: false,
-	}
+	};
 
 	render() {
 		let style = {
-			'color':'green',
-			'textDecoration':'none',
-			'fontWeight':'bold',
-		}
+			color: "green",
+			textDecoration: "none",
+			fontWeight: "bold",
+		};
 		return (
 			<BrowserRouter>
 				<header>
 					<nav>
 						<div id="navbar">
 							<ul>
-								<li className="chrome"><Link onClick={this.loadingHandler} style={style} to="/">Travel Locations</Link></li>
-								<li className="chrome"><Link style={style} to="/all-locations">All Locations</Link></li>
-								<li className="chrome" id="titlebar"><h2 className="chrome-title">React Travel App</h2></li>
+								<li className="chrome">
+									<Link
+										onClick={this.loadingHandler}
+										style={style}
+										to="/"
+									>
+										Travel Locations
+									</Link>
+								</li>
+								<li className="chrome">
+									<Link style={style} to="/all-locations">
+										All Locations
+									</Link>
+								</li>
+								<li className="chrome" id="titlebar">
+									<h2 className="chrome-title">
+										React Travel App
+									</h2>
+								</li>
+								<li className="website">
+									<a
+										style={{
+											textDecoration: "none",
+											color: "green",
+											fontWeight: "bold",
+											fontSize: "regular",
+										}}
+										href="http://localhost:8000"
+									>
+										Go to Website
+									</a>
+								</li>
 							</ul>
 						</div>
 					</nav>
@@ -34,7 +62,14 @@ class App extends Component {
 				<div id="container" className="App">
 					<Switch>
 						<Route path="/all-locations" component={AllLocations} />
-						<Route path="/" component={MyMap}/>
+						{/* <Route
+							path="/website"
+							component={() => {
+								window.location = "http://localhost:8000";
+								return null;
+							}}
+						/> */}
+						<Route path="/" component={MyMap} />
 					</Switch>
 					{/* <MyMap /> */}
 				</div>
--- travel-app-client/src/containers/MyMap/MyMap.js
+++ travel-app-client/src/containers/MyMap/MyMap.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import * as ReactLeaflet from 'react-leaflet';
-import Button from '../../components/UI/Button/Button';
-import Success from '../../components/Success/Success';
-import axios from 'axios';
-import Spinner from '../../components/UI/Spinner/Spinner';
-import classes from './MyMap.module.css';
+import React, { Component } from "react";
+import * as ReactLeaflet from "react-leaflet";
+import Button from "../../components/UI/Button/Button";
+import Success from "../../components/Success/Success";
+import axios from "axios";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import classes from "./MyMap.module.css";
 
-const { Map: LeafletMap, TileLayer, Marker, Popup } = ReactLeaflet
+const { Map: LeafletMap, TileLayer, Marker, Popup } = ReactLeaflet;
 
 class MyMap extends Component {
 	constructor() {
@@ -22,7 +22,7 @@ class MyMap extends Component {
 			place_id: null,
 			success: null,
 			uploading: false,
-			opacity: 1
+			opacity: 1,
 		};
 	}
 	// state = {
@@ -32,134 +32,143 @@ class MyMap extends Component {
 	// }
 
 	getLocation = () => {
-		if ('geolocation' in navigator) {
+		if ("geolocation" in navigator) {
 			// console.log('geolocation available');
 			navigator.geolocation.getCurrentPosition(async position => {
 				let lat = position.coords.latitude.toFixed(4);
 				let lng = position.coords.longitude.toFixed(4);
-				document.getElementById('lat').innerText = lat;
-				document.getElementById('lng').innerText = lng;
-				 // [lat, lon] = [37.636845, -118.986252]; //Mammoth Mtn
+				document.getElementById("lat").innerText = lat;
+				document.getElementById("lng").innerText = lng;
+				// [lat, lon] = [37.636845, -118.986252]; //Mammoth Mtn
 				let geocodingUrl = `geocode-location/${lat},${lng}`;
 				// console.log(geocodingUrl);
 				let loading = this.state.loading;
 				loading = (
 					<Popup
 						className={classes.Popup}
-						position={[lat,lng]}
+						position={[lat, lng]}
 						onOpen={this.loadingHandler}
-						onClose={this.finishLoadingHandler}>
-						{<Spinner/>}
+						onClose={this.finishLoadingHandler}
+					>
+						{<Spinner />}
 					</Popup>
 				);
-				this.setState({loading:loading});
+				this.setState({ loading: loading });
 				const response = await axios.get(geocodingUrl);
-				this.setState({loading:null})
+				this.setState({ loading: null });
 				// console.log(response.data.results[0].formatted_address);
 				// console.log(response);
 				const address = response.data.results[0].formatted_address;
 				console.log(response.data.results[0].address_components);
 				// const shortName = response.data.results[0].address_components[0].long_name;
-				let temp = address.split(',');
+				let temp = address.split(",");
 				let shortName;
 				//! street, location, state/zip, country
-				if(temp.length === 4){
+				if (temp.length === 4) {
 					shortName = temp[1]; //location
 				}
 				//! location, state/zip, country
-				else if(temp.length < 4) {
+				else if (temp.length < 4) {
 					shortName = temp[0];
 				}
 				const id = response.data.results[0].place_id;
 				// console.log(lat, lng);
-				this.setState({lat:lat,lng:lng,locName:address, place_id: id,shortName:shortName});
+				this.setState({
+					lat: lat,
+					lng: lng,
+					locName: address,
+					place_id: id,
+					shortName: shortName,
+				});
 				setTimeout(() => {
 					// console.log('setting state in getLocation()');
-					if(this.state.markers.length < 1)
-						this.setMarker();
-					else 
-						this.moveMarker();
+					if (this.state.markers.length < 1) this.setMarker();
+					else this.moveMarker();
 				}, 1);
-			})
+			});
 		} else {
-			console.log('geolocation not available');
+			console.log("geolocation not available");
 		}
-	}
+	};
 
 	setMarker = () => {
 		let position = [this.state.lat, this.state.lng];
-		let x = (
-			<Marker position={position} key={this.state.counter} />
-		);
+		let x = <Marker position={position} key={this.state.counter} />;
 		let markers = [...this.state.markers];
 		markers.push(x);
-		this.setState({markers:markers});
-	}
+		this.setState({ markers: markers });
+	};
 
 	moveMarker = () => {
 		let position = [this.state.lat, this.state.lng];
-		let x = <Marker position={position} key={this.state.counter} />
+		let x = <Marker position={position} key={this.state.counter} />;
 		let markers = [this.state.markers];
 		markers.pop();
 		markers.push(x);
-		this.setState({markers:markers});
-	}
-	
+		this.setState({ markers: markers });
+	};
 
 	setLocation = (lat, lng, name, id, shortName) => {
-		this.setState({lat:lat,lng:lng,locName:name,place_id:id,shortName:shortName});
-		if(this.state.markers.length < 1) {
+		this.setState({
+			lat: lat,
+			lng: lng,
+			locName: name,
+			place_id: id,
+			shortName: shortName,
+		});
+		if (this.state.markers.length < 1) {
 			this.setMarker();
 		} else {
 			this.moveMarker();
 		}
-	}
+	};
 
 	onSearchHandler = async () => {
-			let data = document.getElementById('searchbox');
-			// console.log(data.value);
-			let nice = data.value.split(' ');
-			// console.log(nice);
-			let address = nice[0];
-			for(let i = 1; i < nice.length; ++i) {
-				address += '+' + nice[i];
+		let data = document.getElementById("searchbox");
+		// console.log(data.value);
+		let nice = data.value.split(" ");
+		// console.log(nice);
+		let address = nice[0];
+		for (let i = 1; i < nice.length; ++i) {
+			address += "+" + nice[i];
+		}
+		// console.log(address);
+		let url = `geocode-address/${address}`;
+		let loading = this.state.loading;
+		loading = (
+			<Popup
+				className={classes.Popup}
+				position={[this.state.lat, this.state.lng]}
+				onOpen={this.loadingHandler}
+				onClose={this.finishLoadingHandler}
+			>
+				{<Spinner />}
+			</Popup>
+		);
+		this.setState({ loading: loading });
+		const response = await axios.get(url);
+		this.setState({ loading: null });
+		// console.log(response);
+		let lat, lng, name, place_id, shortName;
+		if (response.data) {
+			lat = response.data.results[0].geometry.location.lat;
+			lng = response.data.results[0].geometry.location.lng;
+			name = response.data.results[0].formatted_address;
+			place_id = response.data.results[0].place_id;
+			console.log(response.data.results);
+			let temp = name.split(",");
+			//! street, location, state/zip, country
+			if (temp.length === 4) {
+				shortName = temp[1]; //location
 			}
-			// console.log(address);
-			let url = `geocode-address/${address}`;
-			let loading = this.state.loading;
-			loading = (
-				<Popup
-					className={classes.Popup}
-					position={[this.state.lat,this.state.lng]}
-					onOpen={this.loadingHandler}
-					onClose={this.finishLoadingHandler}>
-					{<Spinner/>}
-				</Popup>
-			);
-			this.setState({loading:loading});
-			const response = await axios.get(url);
-			this.setState({loading:null})
-			// console.log(response);
-			let lat, lng, name, place_id, shortName;
-			if(response.data) {
-				lat = response.data.results[0].geometry.location.lat;
-				lng = response.data.results[0].geometry.location.lng;
-				name = response.data.results[0].formatted_address;
-				place_id = response.data.results[0].place_id;
-				console.log(response.data.results)
-				let temp = name.split(',');
-				//! street, location, state/zip, country
-				if(temp.length === 4){
-					shortName = temp[1]; //location
-				}
-				//! location, state/zip, country
-				else if(temp.length < 4) {
-					shortName = temp[0];
-				}
-				// shortName = response.data.results[0].address_components[0].long_name;
-				this.setLocation(lat, lng, name, place_id, shortName);
-			} 	
-	}
+			//! location, state/zip, country
+			else if (temp.length < 4) {
+				shortName = temp[0];
+			}
+			// shortName = response.data.results[0].address_components[0].long_name;
+			this.setLocation(lat, lng, name, place_id, shortName);
+		}
+	};
 
 	clearState = () => {
 		let markers = [this.state.markers];
@@ -169,97 +178,135 @@ class MyMap extends Component {
 		name = null;
 		let c = this.state.counter;
 		c = 1;
-		for(let i = 0; i < markers.length; ++i) {
+		for (let i = 0; i < markers.length; ++i) {
 			markers.pop();
 		}
-		this.setState({place_id:id,locName:name,counter:c,markers:markers});
+		this.setState({
+			place_id: id,
+			locName: name,
+			counter: c,
+			markers: markers,
+		});
 		// console.log('State cleared');
-	}
+	};
 
 	onSendHandler = async () => {
-			if(this.state.markers.length < 1) return;
-			let lat = this.state.lat;
-			let lng = this.state.lng;
-            const time = Date.now();
-            const locName = this.state.locName;
-			const place_id = this.state.place_id;
-			const entryNum = this.state.counter;
-			const shortName = this.state.shortName;
-            const data = {locName, shortName, place_id, lat, lng, time, entryNum};
-			console.log('fetching');
-			let loading = this.state.loading;
-			loading = (
-				<Popup
-					className={classes.Popup}
-					position={[lat,lng]}
-					onOpen={this.loadingHandler}
-					onClose={this.finishLoadingHandler}>
-					{<Spinner/>}
-				</Popup>
-			);
-			this.setState({loading:loading});
-			const response = await axios.post('/api', data);
-			console.log(response);
-			this.clearState();
-			let success = this.state.success;
-			// success = <Success />;
-			success = <Spinner/>;
-			this.setState({success:success, counter: this.state.counter + 1, loading:null});
-			setTimeout(() => {
-				let s = this.state.success;
-				s = null;
-				this.setState({success:s});
-			}, 1000);
-	}
+		if (this.state.markers.length < 1) return;
+		let lat = this.state.lat;
+		let lng = this.state.lng;
+		const time = Date.now();
+		const locName = this.state.locName;
+		const place_id = this.state.place_id;
+		const entryNum = this.state.counter;
+		const shortName = this.state.shortName;
+		const data = { locName, shortName, place_id, lat, lng, time, entryNum };
+		console.log("fetching");
+		let loading = this.state.loading;
+		loading = (
+			<Popup
+				className={classes.Popup}
+				position={[lat, lng]}
+				onOpen={this.loadingHandler}
+				onClose={this.finishLoadingHandler}
+			>
+				{<Spinner />}
+			</Popup>
+		);
+		this.setState({ loading: loading });
+		const response = await axios.post("/api", data);
+		console.log(response);
+		this.clearState();
+		let success = this.state.success;
+		// success = <Success />;
+		success = <Spinner />;
+		this.setState({
+			success: success,
+			counter: this.state.counter + 1,
+			loading: null,
+		});
+		setTimeout(() => {
+			let s = this.state.success;
+			s = null;
+			this.setState({ success: s });
+		}, 1000);
+	};
 
 	onZoomHandler = () => {
-		if(this.state.markers.length < 1) return;
+		if (this.state.markers.length < 1) return;
 		let zoom = this.state.zoom;
 		zoom = 12;
-		this.setState({zoom:zoom});
-	}
+		this.setState({ zoom: zoom });
+	};
 
-  render() {
-    // const position = [this.state.lat, this.state.lng];
-    return (
-		<div>
-			<h2>Travel Locations</h2>
-			<LeafletMap 
-				center={[this.state.lat, this.state.lng]}
-				zoom={this.state.zoom} 
-				style={{'width':'80%', 'position':'relative', 'left':'10%', 'border':'1px solid hotpink'}}
+	render() {
+		// const position = [this.state.lat, this.state.lng];
+		return (
+			<div>
+				<h2>Travel Locations</h2>
+				<LeafletMap
+					center={[this.state.lat, this.state.lng]}
+					zoom={this.state.zoom}
+					style={{
+						width: "80%",
+						position: "relative",
+						left: "10%",
+						border: "1px solid hotpink",
+					}}
 				>
-				<TileLayer
-				opacity={this.state.opacity}
-				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-				url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-				/>
-				{/* <Marker position={position}>
+					<TileLayer
+						opacity={this.state.opacity}
+						attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+					/>
+					{/* <Marker position={position}>
 				<Popup>
 					NICE
 				</Popup>
 				</Marker> */}
-				{this.state.loading}
-				{this.state.markers}
-			</LeafletMap>
-			{/* {this.state.success} */}
-			<Button disabled={false} clicked={this.getLocation}>Find Location</Button>
-			<Button disabled={false} clicked={this.onSendHandler}>Send It!</Button>
-			<Button disabled={false} clicked={this.onZoomHandler}>Zoom
-			In</Button>
-			<h2>Or Search For A Location Below</h2>
-			<form id="searchForm" type="text" onSubmit={this.onSearchHandler}>
-            	<input type="text" style={{'width':'20%'}} id="searchbox" name="serach"/>
-            	<br/>
-            	<button onClick={this.onSearchHandler} type="button" id="send" style={{'marginTop': '10px'}}>Search</button>
-        	</form>
-			<p style={{'fontWeight':'bold', 'fontSize':'larger'}}>Latitude: <span id="lat">{this.state.lat}</span></p>
-			<p style={{'fontWeight':'bold', 'fontSize':'larger'}}>Longitude: <span id="lng">{this.state.lng}</span></p>
-		</div>
-      
-
-    );
-  }
+					{this.state.loading}
+					{this.state.markers}
+				</LeafletMap>
+				{/* {this.state.success} */}
+				<Button disabled={false} clicked={this.getLocation}>
+					Find Location
+				</Button>
+				<Button disabled={false} clicked={this.onSendHandler}>
+					Send It!
+				</Button>
+				<Button disabled={false} clicked={this.onZoomHandler}>
+					Zoom In
+				</Button>
+				<h2>Or Search For A Location Below</h2>
+				<form
+					id="searchForm"
+					type="text"
+					onSubmit={this.onSearchHandler}
+				>
+					<input
+						type="text"
+						style={{ width: "20%" }}
+						id="searchbox"
+						name="serach"
+					/>
+					<br />
+					<button
+						onClick={this.onSearchHandler}
+						type="button"
+						id="send"
+						style={{ marginTop: "10px" }}
+					>
+						Search
+					</button>
+				</form>
+				<p style={{ fontWeight: "bold", fontSize: "larger" }}>
+					Latitude: <span id="lat">{this.state.lat}</span>
+				</p>
+				<p style={{ fontWeight: "bold", fontSize: "larger" }}>
+					Longitude: <span id="lng">{this.state.lng}</span>
+				</p>
+			</div>
+		);
+	}
 }
 
 export default MyMap;
